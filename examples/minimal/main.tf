6531e0af93331e508f893e6d380aea44b1a4d32e
Christopher Basketfield <chris.basketfield@gmail.com>
2021-12-12 21:32:55 -0900

    minimizing

--- examples/minimal/main.tf
+++ examples/minimal/main.tf
@@ -1,9 +1,7 @@
 module "my_eks_cluster" {
   source = "../../"
 
-  name               = "my-eks-cluster"
-  kubernetes_version = "1.21"
-
+  name = "my-eks-cluster"
   vpc_config = {
     subnet_ids = [
       "subnet-345345",
