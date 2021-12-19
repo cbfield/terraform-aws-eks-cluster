7e100ed6fdc10b62dcc04921aa5898ba491c6046
Christopher Basketfield <chris.basketfield@gmail.com>
2021-12-18 16:31:12 -0900

    oidc provider creation

--- _data.tf
+++ _data.tf
@@ -1,3 +1,9 @@
 data "aws_eks_cluster_auth" "auth" {
   name = aws_eks_cluster.cluster.name
 }
+
+data "tls_certificate" "cluster" {
+  count = coalesce(try(var.iam.create_oidc_provider, null), true) ? 1 : 0
+
+  url = aws_eks_cluster.cluster.identity[0].oidc[0].issuer
+}
